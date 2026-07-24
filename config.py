5c14a52433b3a19eafad73746aef9f74fd68ac23
praisedeveloper15-spec <praisedeveloper15@gmail.com>
2026-07-24 10:15:58 -0700

    Update config.py

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ load_dotenv()
 
 BOT_TOKEN = os.environ["BOT_TOKEN"]
 DATABASE_URL = os.environ["DATABASE_URL"].replace("postgres://", "postgresql://", 1)
-DATAFORSEO_LOGIN = os.environ["DATAFORSEO_LOGIN"]
-DATAFORSEO_PASSWORD = os.environ["DATAFORSEO_PASSWORD"]
+
+SERPAPI_KEY = os.environ["SERPAPI_KEY"]
 
 DAILY_CHECK_HOUR_UTC = int(os.environ.get("DAILY_CHECK_HOUR_UTC", "3"))
 MAX_KEYWORDS_PER_PROJECT = int(os.environ.get("MAX_KEYWORDS_PER_PROJECT", "50"))
